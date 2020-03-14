f0a13c47a8730b23f71dfe01d5acbbd70dad38e3
Yury Kundin <yury.kundin@yandex.ru>
2020-03-14 21:08:44 +0300

    Пересел с иглы BEM на CSS Modules

--- webpack/webpack.node.js
+++ webpack/webpack.node.js
@@ -3,6 +3,9 @@ const merge = require('webpack-merge');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const LoadablePlugin = require('@loadable/webpack-plugin');
 const nodeExternals = require('webpack-node-externals');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const postcssNested = require('postcss-nested');
+const postcssPresetEnv = require('postcss-preset-env');
 const dotenv = require('dotenv');
 
 const config = require('./webpack.common');
@@ -32,7 +35,40 @@ module.exports = merge(config, {
       {
         test: /\.css$/,
         exclude: /node_modules/,
-        use: 'null-loader',
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader,
+            options: {
+              hmr: !isProd,
+            },
+          },
+          {
+            loader: 'css-loader',
+            options: {
+              modules: {
+                localIdentName: isProd
+                  ? '[hash:base64:5]'
+                  : '[path][name]__[local]--[hash:base64:5]',
+              },
+              importLoaders: 1,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              plugins: [
+                postcssNested(),
+                postcssPresetEnv({
+                  stage: 1,
+                  autoprefixer: {
+                    flexbox: 'no-2009',
+                  },
+                }),
+              ],
+            },
+          },
+        ],
       },
       {
         test: /\.(png|svg|jpg|gif|woff|woff2)$/,
@@ -48,6 +84,14 @@ module.exports = merge(config, {
     publicPath: path.resolve(__dirname, '../dist/node/'),
     libraryTarget: 'commonjs2',
   },
-  externals: ['@loadable/component', nodeExternals()],
-  plugins: [new CleanWebpackPlugin(), new LoadablePlugin()],
+  externals: ['@loadable/component', nodeExternals({ whitelist: /\.css$/ })],
+  plugins: [
+    new CleanWebpackPlugin(),
+    new LoadablePlugin(),
+    new MiniCssExtractPlugin({
+      ignoreOrder: true,
+      filename: '[id].[contenthash].css',
+      chunkFilename: '[id].[hash].css',
+    }),
+  ],
 });
